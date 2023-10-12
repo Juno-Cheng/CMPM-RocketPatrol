0d3736b696540d5fd4bd572740a31b414cb7fcb4
Juno-Cheng <chengjonathan2021@gmail.com>
2023-10-11 21:12:37 -0700

    Added Config

--- Rocket Patrol/src/main.js
+++ Rocket Patrol/src/main.js
@@ -1,4 +1,10 @@
 //The console.log() function allows us to “print” messages to the JavaScript Console, a browser tool provided to developers 
 //to help debug their code. The Console is essential to web programming, and you’ll use it extensively while building web games.
 
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480
+  }
+  
 let game = new Phaser.Game();

6bf96c0f40661f2490f2f87ebfc7e085ba152e78
Juno-Cheng <chengjonathan2021@gmail.com>
2023-10-11 17:11:28 -0700

    JS Added

---
+++ Rocket Patrol/src/main.js
@@ -0,0 +1,2 @@
+console.log("hello world");
+//The console.log() function allows us to “print” messages to the JavaScript Console, a browser tool provided to developers to help debug their code. The Console is essential to web programming, and you’ll use it extensively while building web games.

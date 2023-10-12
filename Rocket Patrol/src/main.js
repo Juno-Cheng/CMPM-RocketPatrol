b9bb7028018390ebfd466bfd1b89b3ee89d8f573
Juno-Cheng <chengjonathan2021@gmail.com>
2023-10-11 20:03:25 -0700

    Fixed Location

--- Rocket Patrol/src/main.js
+++ Rocket Patrol/src/main.js
@@ -1,2 +1,4 @@
-console.log("hello world");
-//The console.log() function allows us to “print” messages to the JavaScript Console, a browser tool provided to developers to help debug their code. The Console is essential to web programming, and you’ll use it extensively while building web games.
+//The console.log() function allows us to “print” messages to the JavaScript Console, a browser tool provided to developers 
+//to help debug their code. The Console is essential to web programming, and you’ll use it extensively while building web games.
+
+let game = new Phaser.Game();

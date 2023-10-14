1445b2bd08f9425344e306fa4e97ce9235e62fcd
Juno-Cheng <chengjonathan2021@gmail.com>
2023-10-13 22:55:56 -0700

    Green UI

--- Rocket Patrol/src/main.js
+++ Rocket Patrol/src/main.js
@@ -14,3 +14,5 @@ let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
+//Green UI
+this.add.rectangle(0, borderUISize+borderPadding, game.config.width)

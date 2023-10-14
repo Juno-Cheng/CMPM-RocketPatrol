f3605f173f6372055129f35decceac0dd7a666fb
Juno-Cheng <chengjonathan2021@gmail.com>
2023-10-14 16:43:21 -0700

    Add Logs

--- Rocket Patrol/src/scenes/Menu.js
+++ Rocket Patrol/src/scenes/Menu.js
@@ -4,6 +4,7 @@ class Menu extends Phaser.Scene {
     }
     
     create() {
+      console.log("Menu scene activated");
       this.add.text(20, 20, "Rocket Patrol Menu");
       this.scene.start("playScene");
     }
--- Rocket Patrol/src/scenes/Play.js
+++ Rocket Patrol/src/scenes/Play.js
@@ -4,11 +4,14 @@
 
 class Play extends Phaser.Scene {
     constructor() {
-      super("playScene");
+        super("playScene");
     }
     
     create() {
-      //Green UI - Starting Point (0, borderUISize + borderPadding), X Leng (Width), Y Height (borderUISize * 2) 
+        
+    console.log("Menu scene activated");
+    this.add.text(20, 20, "Rocket Patrol Play");
+    //Green UI - Starting Point (0, borderUISize + borderPadding), X Leng (Width), Y Height (borderUISize * 2) 
     //.setOrigin is Weird - (0,0) sets to topLeft
     this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
 
@@ -17,5 +20,5 @@ class Play extends Phaser.Scene {
     this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
     this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
     this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        }
+    }
 } 

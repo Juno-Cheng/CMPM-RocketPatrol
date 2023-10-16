b391670dbc72f8be01e1009321c0cbb7dccee1ee
Juno-Cheng <chengjonathan2021@gmail.com>
2023-10-16 00:43:50 -0700

    Added Images

--- Rocket Patrol/src/scenes/Play.js
+++ Rocket Patrol/src/scenes/Play.js
@@ -7,18 +7,32 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
     
-    create() {
-        
-    console.log("Menu scene activated");
-    this.add.text(20, 20, "Rocket Patrol Play");
-    //Green UI - Starting Point (0, borderUISize + borderPadding), X Leng (Width), Y Height (borderUISize * 2) 
-    //.setOrigin is Weird - (0,0) sets to topLeft
-    this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+    preload() {
+        // load images/tile sprites
+        //this class, load image, name, location
+        //The load.image() method expects two parameters: a string with the key name of the graphic 
+        //you’re going to use (so you can reference it later in your program) and the URL for where your graphic is located.
 
-    //White Borders:
-    this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
+    create() {
+        console.log("Play scene activated");
+        this.add.text(20, 20, "Rocket Patrol Play");
+        //Green UI - Starting Point (0, borderUISize + borderPadding), X Leng (Width), Y Height (borderUISize * 2) 
+        //.setOrigin is Weird - (0,0) sets to topLeft
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+
+        //In Play.js's create() method, add the following code above the code that places the rectangular borders:
+        
+
+        //White Borders: 
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0); 
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        //game.config.width - borderUISize Make space for the border
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
     }
 } 
